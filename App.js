9b47c91ee4d5a78d73e309877544e467181b9098
georgeussher <gussher@icloud.com>
2023-07-22 16:41:58 +0100

    Logo SVG added

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  Logo  from './components/logo.js';
+import React from 'react';
+// import Svg, { Path, } from 'react-native-svg';
+// import './styles.css';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Lets Go!!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+    <Logo/>  
+    
+    <Text>Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
     </View>
   );
 }
