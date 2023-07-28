69f6111c01426b367b82d01dd07c7ba7e05fe7d8
georgeussher <gussher@icloud.com>
2023-07-28 14:38:08 +0100

    task2 branch created

--- App.js
+++ App.js
@@ -3,27 +3,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import  Logo  from './components/Logo.js';
 import AppButton from './components/Button.js';
-// import Svg, { Path, } from 'react-native-svg';
-// import './styles.css';
+
  
 
 export default function App() {
   return (
     <View style={styles.container}> 
-    <Logo /> 
-    <View style={styles.buttonContainer}>
-    <AppButton
-      backgroundColor={"#FE07C8"} 
-      title={'Login'}
-      borderColor={"#A010A3"}
-      />
-    <AppButton
-      backgroundColor={"#A010A3"} 
-      title={'Signup'}
-      borderColor={"#FE07C8"}
-      />
-    </View>
-    <StatusBar style="auto" />
+  
     </View>
   );
 }
