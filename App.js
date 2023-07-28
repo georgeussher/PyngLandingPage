6464a3504e0fdc023b74007a0707171bbe729f27
georgeussher <gussher@icloud.com>
2023-07-28 15:17:26 +0100

    navbar color added

--- App.js
+++ App.js
@@ -7,25 +7,25 @@ import NavbarContainer from './components/navbar';
 
 export default function App() {
   return (
-    <View > 
-      <NavbarContainer style={StyleSheet.container}/>
+    <View styles={styles.buttonContainer} > 
+      <NavbarContainer />
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: "space-around",
-//     alignItems: "center",
-//     backgroundColor: "#f0f0f0",
-//     marginTop: 150,
-//   },
-//   buttonContainer: {
-//     flex: 1,
-//     justifyContent: "flex-end",
-//     alignItems: "center",
-//     marginBottom: 36,
-//   },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginTop: 150,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 36,
+    backgroundColor: "#E010A5",
+  },
  
-//});
+});
--- components/navbar.js
+++ components/navbar.js
@@ -3,25 +3,22 @@ import { StyleSheet, View, Text} from "react-native";
 
 export default function NavbarContainer(){
 
-    const styles = StyleSheet.create({
-        navbarContainer: {
-          width: 271,
-          height: 44,
-          elevation: 8,
-          backgroundColor: "#A010A3",
-          color: "#A010A3",
-          borderRadius: 50,
-          paddingVertical: 10,
-          paddingHorizontal: 12,
-        //   borderColor: borderColor,
-          borderWidth: 1,
-          marginVertical: 8,
-        },
-      });
-
+    
     return (
-        <View style={styles.Navbarcontainer}>
-            <Text>Navbar</Text>
+        <View style={styles.NavbarContainer}>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    NavbarContainer: {
+      width: 280,
+      height: 39,
+      elevation: 8,
+      backgroundColor: "#A010A3",
+      color: "#A010A3",
+      borderRadius: 20,
+      paddingVertical: 10,
+      paddingHorizontal: 50,
+      marginVertical: 8,
+    },
+  });
