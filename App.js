7cbe1f66a48b43655c521757498d1760c2ed6f36
georgeussher <gussher@icloud.com>
2023-08-02 17:29:56 +0100

    formatted master code

--- App.js
+++ App.js
@@ -1,46 +1,42 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import  Logo  from './components/logo';
+import Logo from './components/logo';
 import AppButton from './components/Button.js';
-// import Svg, { Path, } from 'react-native-svg';
-// import './styles.css';
- 
 
 export default function App() {
-  return (
-    <View style={styles.container}> 
-    <Logo /> 
-    <View style={styles.buttonContainer}>
-    <AppButton
-      backgroundColor={"#FE07C8"} 
-      title={'Login'}
-      borderColor={"#A010A3"}
-      />
-    <AppButton
-      backgroundColor={"#A010A3"} 
-      title={'Signup'}
-      borderColor={"#FE07C8"}
-      />
-    </View>
-    <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Logo />
+			<View style={styles.buttonContainer}>
+				<AppButton
+					backgroundColor={'#FE07C8'}
+					title={'Login'}
+					borderColor={'#A010A3'}
+				/>
+				<AppButton
+					backgroundColor={'#A010A3'}
+					title={'Signup'}
+					borderColor={'#FE07C8'}
+				/>
+			</View>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "space-around",
-    alignItems: "center",
-    backgroundColor: "#FFF",
-    marginTop: 150,
-  },
-  buttonContainer: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    marginBottom: 36,
-  },
- 
+	container: {
+		flex: 1,
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		backgroundColor: '#FFF',
+		marginTop: 150,
+	},
+	buttonContainer: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		marginBottom: 36,
+	},
 });
--- components/Button.js
+++ components/Button.js
@@ -1,38 +1,36 @@
-import * as React from "react"
-import { StyleSheet, Pressable, Text } from "react-native";
+import * as React from 'react';
+import { StyleSheet, Pressable, Text } from 'react-native';
 
-export default function AppButton({title, backgroundColor, borderColor}){
+export default function AppButton({ title, backgroundColor, borderColor }) {
+	const handleButtonPress = () => {
+		console.log('Button pressed!');
+	};
 
-  const handleButtonPress = () => {
-    console.log("Button pressed!");
-  };
+	const styles = StyleSheet.create({
+		appButtonContainer: {
+			width: 271,
+			height: 44,
+			elevation: 8,
+			backgroundColor: backgroundColor,
+			borderRadius: 50,
+			paddingVertical: 10,
+			paddingHorizontal: 12,
+			borderColor: borderColor,
+			borderWidth: 1,
+			marginVertical: 8,
+		},
+		appButtonText: {
+			fontSize: 18,
+			color: '#fff',
+			fontWeight: 'bold',
+			alignSelf: 'center',
+			textTransform: 'uppercase',
+		},
+	});
 
-  const styles = StyleSheet.create({
-    appButtonContainer: {
-      width: 271,
-      height: 44,
-      elevation: 8,
-      backgroundColor: backgroundColor,
-      borderRadius: 50,
-      paddingVertical: 10,
-      paddingHorizontal: 12,
-      borderColor: borderColor,
-      borderWidth: 1,
-      marginVertical: 8,
-    },
-    appButtonText: {
-      fontSize: 18,
-      color: "#fff",
-      fontWeight: "bold",
-      alignSelf: "center",
-      textTransform: "uppercase"
-    }
-  });
-
-    return (
-    <Pressable onPress={handleButtonPress} style={styles.appButtonContainer} >
-    <Text style={styles.appButtonText}>{title}</Text>
-    </Pressable>
-    )
-};
-
+	return (
+		<Pressable onPress={handleButtonPress} style={styles.appButtonContainer}>
+			<Text style={styles.appButtonText}>{title}</Text>
+		</Pressable>
+	);
+}
